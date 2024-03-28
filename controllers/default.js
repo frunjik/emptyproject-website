c58eb5f1ee024a40bba9371f79974948a6a0cd99
frunjik <frunjik@gmail.com>
2024-03-28 07:18:21 +0100

    updating

--- controllers/default.js
+++ controllers/default.js
@@ -1,16 +1,8 @@
 exports.install = function() {
 
-	ROUTE('/');
-	ROUTE('/services/', view_services);
-	ROUTE('/contact/');
-	ROUTE('/references/');
+	ROUTE('/*');
 
 	// Enables a localization mechanism + compression for all client-side components
 	LOCALIZE('/components/*.html', ['compress']);
 
 };
-
-function view_services() {
-	var self = this;
-	self.view('services');
-}
